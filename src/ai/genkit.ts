40dbf09284b0e9a222aa0044a12835be1b2aa88c
Sukhdev Rawat <sukhdevrawatkhonkar@gmail.com>
2025-08-28 16:27:26 +0000

    Fix: Vercel build error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [
-    googleAI({ aihub: true })
+    googleAI(),
+    firebase(),
   ],
   logLevel: "debug",
   enableTracingAndMetrics: false,
