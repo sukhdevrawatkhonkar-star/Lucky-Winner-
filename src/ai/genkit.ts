db3424e7522146a978b4cab65d56006db5eb8cef
Sukhdev Rawat <sukhdevrawatkhonkar@gmail.com>
2025-08-29 18:23:46 +0000

    [23:49:54.994] Running build in Washington, D.C., USA (East) – iad1 [23:

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,12 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebasePlugin} from '@genkit-ai/firebase';
+import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    firebasePlugin(),
+    firebase(),
   ],
   logLevel: "debug",
-  enableTracingAndMetrics: false,
+  enableTracingAndMetrics: true,
 });
