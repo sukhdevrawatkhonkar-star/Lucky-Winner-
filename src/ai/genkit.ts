7d9a9b8619010173c4da5793819c3724b52eeedd
Sukhdev Rawat <sukhdevrawatkhonkar@gmail.com>
2025-08-29 09:07:45 +0000

    Fix Genkit build error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase';
+import {firebasePlugin} from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    firebase(),
+    firebasePlugin(),
   ],
   logLevel: "debug",
   enableTracingAndMetrics: false,
