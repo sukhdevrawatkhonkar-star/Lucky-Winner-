f55d95e46715eec86931ac7a15392d8526bc272d
Sukhdev Rawat <sukhdevrawatkhonkar@gmail.com>
2025-09-06 19:46:03 +0000

    Pahle apni application ko start to karo mujhe batao to sahi ismein क्या-

--- src/app/agent/layout.tsx
+++ src/app/agent/layout.tsx
@@ -10,7 +10,7 @@ export default function AgentLayout({
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <AgentSidebar />
       <div className="flex flex-col">
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 pt-20">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           {children}
         </main>
       </div>
